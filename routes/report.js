0cc4ec3365f7fcb984ac0446bdb8584872b2ed6f
Zedd Michael <Zedd Michael>
2016-04-17 01:47:35 -0500

    added report upset

--- routes/report.js
+++ routes/report.js
@@ -38,11 +38,43 @@ function getReports(req, res) {
     });
 }
 
+function updateReport(req, res) {
+    var query = {
+        'name': req.body.name
+    };
+    req.newData = {};
+
+    if (req.body.lng != undefined && req.body.lat != undefined) {
+        req.newData.loc = {
+            type: "Point"
+            , coordinates: [req.body.lng, req.body.lat]
+        }
+    }
+    if (req.body.type != undefined) req.newData.type = req.body.type;
+    if (req.body.status != undefined) req.newData.status = req.body.status;
+    if (req.body.transcript != undefined) req.newData.transcript = req.body.transcript;
+    if (req.body.numFire != undefined) req.newData.numFire = req.body.numFire;
+    if (req.body.numPolice != undefined) req.newData.numPolice = req.body.numPolice;
+    if (req.body.numMedic != undefined) req.newData.numMedic = req.body.numMedic;
+    if (req.body.user != undefined) req.newData.user = req.body.user;
+
+    Report.findOneAndUpdate(query, req.newData, {
+        upsert: true
+    }, function (err, doc) {
+        if (err) return res.send(500, {
+            error: err
+        });
+        return res.status(200).send("succesfully saved");
+    });
+}
+
 
 
 router.post('/', addNewReport);
 
 router.get('/', getReports);
+
+router.post('/updateReport/', updateReport);
 
 
 
